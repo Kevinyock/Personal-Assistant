7ca3f569928b231b477387cf7c8f281627ed8f2f
KevinTheGreat <Westminsterkevindo@gmail.com>
2022-09-18 17:10:13 -0700

    Moved initialization into its own function

--- Main.py
+++ Main.py
@@ -1,19 +1,25 @@
+# Import Libraries
 import pyfiglet
 from gtts import gTTS
 from playsound import playsound 
 
 figletFont = pyfiglet.Figlet()
 
-def main():
-
+def assistantInitlization():
+    
     print(figletFont.renderText("Initializing"))
     print(figletFont.renderText("Primary"))
     print(figletFont.renderText("System"))
 
     assistantVoice = gTTS("Initializing Primary System")
-    #assistantVoice.save
+    assistantVoice.save("Audio/initialization.mp3")
+    playsound("Audio/initialization.mp3")
+
+def main():
+    assistantInitlization()
 
     return 0
+
 
 if __name__ == "__main__":
     main()
