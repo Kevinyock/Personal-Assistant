02a89e2860630e21e16750930d4dcd627f6f93b8
KevinTheGreat <Westminsterkevindo@gmail.com>
2022-10-02 21:34:19 -0700

    Facial Recognition now recognize face and show a box around such face

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -3,15 +3,32 @@ import numpy
 
 class FacialRecognition():
     def __init__(self):
-        print("Module Online")
+        print("Facial Recogition Module Online")
         capture = cv2.VideoCapture(0)
+        face_detector = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
+
         # Check if our camera is connected, if not inform user
-        while(capture.isOpened()):
+        if (capture.isOpened()):
             while True:
-                ret,frame=capture.read()
+                # Captures Each Frame
+                ret, frame=capture.read()
+
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                faces = face_detector.detectMultiScale(
+                    gray,
+                    scaleFactor = 1.1,
+                    minNeighbors = 5
+                )
+
+                # Draw a rectangle around the faces
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
                 cv2.imshow('Color',frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+
             capture.release()
             cv2.destroyAllWindows()
         else:
